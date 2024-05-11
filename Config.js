335f1134a26e520e7b6f7c78368e02af026aeaaa
Aryan0866 <140366478+Aryan0866@users.noreply.github.com>
2024-05-11 16:35:59 +0500

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,16 +3,16 @@ const chalk = require('chalk');
 require('dotenv').config();
 
 //contact details
-global.ownernumber = [ process.env.OWNER_NUMBER || "919931122319"],
-global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.ownernumber = [ process.env.OWNER_NUMBER || "923151595788"],
+global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘卂尺ㄚ卂几_尺卂ﾌ卩ㄖㄖㄒ",
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.location = "Pakistan"
 
 global.botname = process.env.BOTNAME || "𝐌𝐀𝐑𝐈𝐀-𝐌𝐃",//name of the bot
 
 //sticker details
-global.stickername = process.env.STICKER || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.stickername = process.env.STICKER || "💙𓆩‎✘rajpoot_bot𓆪_✰",
 global.packname = 'Sticker By'
 global.author = process.env.AUTHOR || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
 //console view/theme
@@ -27,11 +27,11 @@ global.prefa = process.env.PREFIX || ".",
 
 //false=disable and true=enable
 global.welcome = process.env.WELCOME || "false", //auto welcome
-global.autoRecording = false //auto recording
+global.autoRecording = true //auto recording
 global.autoTyping = false //auto typing
 global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
-global.autobio = false //auto update bio
+global.autobio = true //auto update bio
 global.anti212 = true //auto block +212
 global.autoread_status = false //auto view status/story
 
